7c5290b13ce8c83b46f2cbf159903797859866a8
Ronaldo Sosa <131112886+ronaldo-sosa@users.noreply.github.com>
2024-10-25 08:37:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ import pandas as pd
 import gdown
 
 # URL del archivo de Google Sheets
-drive_url = "https://docs.google.com/spreadsheets/d/1xPgPtX41jZOcsvtzrU_ImaTOh-WBcfZ6/export?format=xlsx"  # Cambia el ID al de tu archivo
+drive_url = "https://docs.google.com/spreadsheets/d/1xPgPtX41jZOcsvtzrU_ImaTOh-WBcfZ6/export?format=xlsx
+"  # Cambia el ID al de tu archivo
 
 # Descargar el archivo desde Google Drive y leerlo como DataFrame
 @st.cache_data  # cache_data permite que el archivo se descargue solo una vez
